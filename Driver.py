1bab31894886985225691a3802f840ab0c97d0eb
Chris Campell <ccampell.asu@gmail.com>
2016-02-29 09:41:51 -0500

    Added File Reading Logic and Numpy Import

--- Driver.py
+++ Driver.py
@@ -9,27 +9,23 @@ Driver.py
 import sys
 import traceback
 import json
+import numpy
 from pprint import pprint
 
-def read_file():
-    pass
-
-
 def main(cmd_args):
+    data = None
     print(cmd_args)
     #input_file = None
     try:
         # input_file = open(cmd_args[1], "a")
-        with open(cmd_args) as input_file:
+        with open(cmd_args[1]) as input_file:
             data = json.load(input_file)
-        pprint(data)
+        for game in data:
+            print(game)
+
     except Exception as error:
         traceback.print_tb(error.__traceback__)
         raise Exception
-
-
-
-
 
 if __name__ == '__main__':
     main(sys.argv)
